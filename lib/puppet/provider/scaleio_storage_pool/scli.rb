ac440ff7e8418552725ef6171871157ace70dd29
Philipp Gassmann <philipp.gassmann1@swisscom.com>
2014-12-18 15:25:25 +0100

    replace tabs with 2 spaces

--- lib/puppet/provider/scaleio_storage_pool/scli.rb
+++ lib/puppet/provider/scaleio_storage_pool/scli.rb
@@ -7,52 +7,52 @@ Puppet::Type.type(:scaleio_storage_pool).provide(:scaleio_storage_pool) do
   confine :osfamily => :redhat
 
   mk_resource_methods
-  
+
   def self.instances
     Puppet.debug('getting instances of storage pools')
-    
+
     # First have to get a list of pdomains
-		pdomains = getProtectionDomains
+    pdomains = getProtectionDomains
 
     pdomain_pools = Hash.new {|h,k| h[k] = [] }
- 
+
     # Get a list of storage pools in each pdomain
     pdomains.each do |pdomain|
       poolNames = []
 
       lines = scli("--query_protection_domain", "--protection_domain_name", pdomain).split("\n")
       lines.each do |line|
-				if line =~/^Storage Pool/
-					poolName = line.split(' ')[2]
-					poolNames << poolName
-				end
+        if line =~/^Storage Pool/
+          poolName = line.split(' ')[2]
+          poolNames << poolName
+        end
       end
       # Build up hash of protection domains and their storage pools
       pdomain_pools[pdomain] << poolNames
     end
-    
-    storage_pool_instances = [] 
-		storage_pool_info = {}
+
+    storage_pool_instances = []
+    storage_pool_info = {}
     # Get detailed info for each storage pool
     pdomain_pools.each do |pdomain, pools|
-      pools.flatten.each do |pool| 
-				spare_policy = ""
+      pools.flatten.each do |pool|
+        spare_policy = ""
 
         pool_query_lines = scli("--query_storage_pool", "--storage_pool_name", pool, "--protection_domain_name", pdomain).split("\n")
         pool_query_lines.each do |line|
           if line =~/Spare policy/
-						spare_pct = line.match /([0-9\.]+%)/
+            spare_pct = line.match /([0-9\.]+%)/
             spare_policy = spare_pct[1];
-          end  
+          end
         end
         # Create storage pools hash
-        new storage_pool_info = { 
-								:name 							=> "#{pdomain}:#{pool}",
-								:pool_name 					=> pool,
-                :ensure 						=> :present,
-                :protection_domain	=> pdomain,
-                :spare_policy 			=> spare_policy,
-				}
+        new storage_pool_info = {
+                :name               => "#{pdomain}:#{pool}",
+                :pool_name           => pool,
+                :ensure             => :present,
+                :protection_domain  => pdomain,
+                :spare_policy       => spare_policy,
+        }
         storage_pool_instances << new(storage_pool_info)
       end
     end
@@ -60,10 +60,10 @@ Puppet::Type.type(:scaleio_storage_pool).provide(:scaleio_storage_pool) do
     storage_pool_instances
   end
 
-	def self.getProtectionDomains
-		pdomains = Puppet::Type.type(:scaleio_protection_domain).instances
-		pdomains.collect {|x| x.parameters[:name].value}
-	end
+  def self.getProtectionDomains
+    pdomains = Puppet::Type.type(:scaleio_protection_domain).instances
+    pdomains.collect {|x| x.parameters[:name].value}
+  end
 
   def self.prefetch(resources)
     Puppet.debug('Prefetching storage pools')
@@ -75,31 +75,30 @@ Puppet::Type.type(:scaleio_storage_pool).provide(:scaleio_storage_pool) do
       end
     end
   end
-  
-  def create 
+
+  def create
     Puppet.debug("Creating storage pool #{@resource[:name]}")
     scli("--add_storage_pool", "--protection_domain_name", @resource[:protection_domain], "--storage_pool_name", @resource[:pool_name])
-		updateSparePolicy(@resource[:spare_policy])
+    updateSparePolicy(@resource[:spare_policy])
     @property_hash[:ensure] = :present
   end
 
   def destroy
-		scli('--remove_storage_pool', '--protection_domain_name', @resource[:protection_domain], '--storage_pool_name', resource[:pool_name])	# TODO: ask if using property hash for removing is correct (resource not working when using purge)
+    scli('--remove_storage_pool', '--protection_domain_name', @resource[:protection_domain], '--storage_pool_name', resource[:pool_name])  # TODO: ask if using property hash for removing is correct (resource not working when using purge)
     @property_hash[:ensure] = :absent
   end
 
-	def spare_policy=(value)
-		updateSparePolicy(value)
-	end
+  def spare_policy=(value)
+    updateSparePolicy(value)
+  end
 
-	def updateSparePolicy(value)
-			Puppet.debug("Updating spare policy of pool #{@resource[:name]} to #{value}")
-	  	result = scli('--modify_spare_policy', '--protection_domain_name', @resource[:protection_domain], '--storage_pool_name', @resource[:pool_name], '--spare_percentage', value, '--i_am_sure')
-	end
-  
+  def updateSparePolicy(value)
+      Puppet.debug("Updating spare policy of pool #{@resource[:name]} to #{value}")
+      result = scli('--modify_spare_policy', '--protection_domain_name', @resource[:protection_domain], '--storage_pool_name', @resource[:pool_name], '--spare_percentage', value, '--i_am_sure')
+  end
+
   def exists?
     Puppet.debug("Cecking existence of storage pool #{@resource[:name]}")
     @property_hash[:ensure] == :present
   end
 end
-
