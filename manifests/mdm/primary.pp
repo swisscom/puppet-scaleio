7387d9b19ef70ca160029dc67ff2df195983891c
Janosch Rohdewald <janosch.rohdewald@swisscom.com>
2015-08-21 11:43:22 +0200

    force string to int conversion :S

--- manifests/mdm/primary.pp
+++ manifests/mdm/primary.pp
@@ -131,7 +131,7 @@ class scaleio::mdm::primary {
   create_resources('scaleio_user',              $scaleio::users,              {ensure => present, require => [Exec['scaleio::mdm::primary_add_secondary'], File[$scaleio::mdm::add_scaleio_user]]})
   create_resources('scaleio_protection_domain', $scaleio::protection_domains, {ensure => present, require => Exec['scaleio::mdm::primary_add_secondary']})
   create_resources('scaleio_storage_pool',      $scaleio::storage_pools,      {ensure => present, require => Exec['scaleio::mdm::primary_add_secondary']})
-  create_resources('scaleio_sds',               $scaleio::sds,                {ensure => present, require => Exec['scaleio::mdm::primary_add_secondary'], tag => 'scaleio_tag_sds', useconsul => $scaleio::use_consul, ramcache_size => $scaleio::ramcache_size})
+  create_resources('scaleio_sds',               $scaleio::sds,                {ensure => present, require => Exec['scaleio::mdm::primary_add_secondary'], tag => 'scaleio_tag_sds', useconsul => $scaleio::use_consul, ramcache_size => $scaleio::ramcache_size + 0})
   create_resources('scaleio_sdc_name',          $scaleio::sdc_names,          {ensure => present, require => [Exec['scaleio::mdm::primary_add_secondary'], Exec['scaleio::mdm::manage_sdc_access_restriction']], tag => 'scaleio_tag_sdc_name', restricted_sdc_mode => $real_restricted_sdc_mode})
   create_resources('scaleio_volume',            $scaleio::volumes,            {ensure => present, require => Exec['scaleio::mdm::primary_add_secondary'], tag => 'scaleio_tag_volume'})
 
